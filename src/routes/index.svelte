3e73f40e727bbc52ac58354a21af0d42169dcd2c
didiermunezer38 <didiermunezer38@gmail.com>
2021-04-19 11:32:43 +0200

    index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,13 @@
-<script>
-	import successkid from 'images/successkid.jpg';
-</script>
+
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
 	h1 {
 		font-size: 2.8em;
 		text-transform: uppercase;
 		font-weight: 700;
-		margin: 0 0 0.5em 0;
 	}
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+	
 
 	p {
 		margin: 1em auto;
@@ -34,17 +18,68 @@
 			font-size: 4em;
 		}
 	}
+	.name{
+	font-family: calibri;
+	width: 100%;
+	position: absolute;
+	left: 20%;
+	top: 50%;
+	transform: translate(-20%,-50%);
+	text-align: center;
+}
+.name p:nth-child(1),.name .details{
+	color:#6c707c;
+	font-size: 16px;
+	font-family: Poppins;
+}
+.name h1{
+	font-family: Hack, monospace;
+	font-size: 40px;
+	margin:0px;
+	letter-spacing: 3px;
+	color:#292929;
+}
+ 
+ 
+.social{
+	left: 50px;
+	bottom: 50px;
+	display: flex;
+	margin: auto;
+	margin-top: 410px;
+    width: 60%;
+    padding: 10px;
+ 
+}
+.social a{
+	margin: 6px 12px;
+ 
+}
+.social i{
+	color:rgba(18,17,17,0.40);
+	font-size: 14px;
+}
+.social a:hover i{
+	color:#ff971d;
+	transition: all ease 0.5s;
+}
 </style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Didier MUNEZERO</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+ <div class="name">
+	<p>Hello there,</p>
+	<h1>I'm Didier MUNEZERO</h1>
+	<!--details--------------->
+	<p class="details">	I am a software engineer at RCA. I like to be a problem solver.</p>
+	
+		
+	</div>
+	<div class="social">
+	<a href="https://www.facebook.com/didiermunezer38/"><i class="fab fa-facebook-f"></i></a>	
+	<a href="https://twitter.com/DidierMUNEZERO4"><i class="fab fa-twitter"></i></a>
+	<a href="https://www.instagram.com/munezerodidier38/"><i class="fab fa-instagram"></i></a>
+	<a href="https://www.linkedin.com/in/didier-munezero-07b849194/"><i class="fab fa-linkedin"></i></a>
+	<a href="https://github.com/didiermunezero"><i class="fab fa-github"></i></a>	
+	</div>
